3f65d5545a6ce8100b9bf0db6bd54b7e4e3725b4
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-07 15:05:38 +0200

    fun random name component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  render() {
-    return (
-      <form className="store-selector">
-        <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
-        <button type="submit">Visit Store →</button>
-      </form>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <form className="store-selector">
+                    <h2>Please Enter A Store</h2>
+                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                    <button type="submit">Visit Store →</button>
+                </form>
+            </React.Fragment>
+        );
+    }
 }
 
 export default StorePicker;
